0106eed0f189db748179b4077d2c76838a6453e6
DuyKhanh <caankhwh1303@gmail.com>
2024-06-20 22:06:16 +0700

    update

--- src/LW.API/Controllers/Public/TopicController.cs
+++ src/LW.API/Controllers/Public/TopicController.cs
@@ -21,42 +21,49 @@ namespace LW.API.Controllers.Public
         {
             _topicService = topicService;
         }
+
         [HttpGet("GetAllTopic")]
         public async Task<ActionResult<ApiResult<TopicDto>>> GetAllLevel()
         {
             var result = await _topicService.GetAll();
             return Ok(result);
         }
+
         [HttpGet("GetTopicById")]
         public async Task<ActionResult<ApiResult<TopicDto>>> GetTopicById(int id)
         {
             var result = await _topicService.GetById(id);
             return Ok(result);
         }
+
         [HttpGet("SearchTopic")]
-        public async Task<ActionResult<IEnumerable<TopicDto>>> SearchTopic([FromQuery] SearchTopicDto searchTopicDto)
+        public async Task<ActionResult<ApiResult<IEnumerable<TopicDto>>>> SearchTopic([FromQuery] SearchTopicDto searchTopicDto)
         {
             var result = await _topicService.SearchTopic(searchTopicDto);
             return Ok(result);
         }
+
         [HttpPost("CreateTopic")]
         public async Task<ActionResult<ApiResult<bool>>> CreateTopic([FromBody] TopicCreateDto model)
         {
             var result = await _topicService.Create(model);
             return Ok(result);
         }
+
         [HttpPut("UpdateTopic")]
         public async Task<ActionResult<ApiResult<bool>>> UpdateTopic([FromBody] TopicUpdateDto model)
         {
             var result = await _topicService.Update(model);
             return Ok(result);
         }
+
         [HttpPut("UpdateStatusTopic/{id}")]
         public async Task<ActionResult<ApiResult<bool>>> UpdateStatusTopic(int id)
         {
             var result = await _topicService.UpdateStatus(id);
             return Ok(result);
         }
+
         [HttpDelete("DeleteTopic/{id}")]
         public async Task<ActionResult<ApiResult<bool>>> DeleteTopic(int id)
         {
