253aef84501e93d623ae975045196a8c8b45af27
Saka289 <saka289@icloud.com>
2024-06-24 01:06:49 +0700

    fix: update validator user

--- src/LW.API/Controllers/Public/UserController.cs
+++ src/LW.API/Controllers/Public/UserController.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using LW.API.Application.Validators.UserValidator;
 using LW.Services.UserService;
 using LW.Shared.DTOs.Facebook;
 using LW.Shared.DTOs.Google;
@@ -33,7 +34,16 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<RegisterResponseUserDto>>> RegisterUser(
             [FromBody] RegisterUserDto registerUserDto)
         {
+            var validationResult = await new CreateUserCommandValidator().ValidateAsync(registerUserDto);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult);
+            }
             var result = await _userService.Register(registerUserDto);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -41,6 +51,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<bool>>> SendVerifyEmail([Required] string email)
         {
             var result = await _userService.SendVerifyEmail(email);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -48,6 +62,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<bool>>> VerifyEmail([Required] string token)
         {
             var result = await _userService.VerifyEmail(token);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -55,6 +73,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<LoginResponseUserDto>>> Login([FromBody] LoginUserDto loginUserDto)
         {
             var result = await _userService.Login(loginUserDto);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -63,6 +85,10 @@ namespace LW.API.Controllers.Public
             [FromBody] GoogleSignInDto googleSignInDto)
         {
             var result = await _userService.LoginGoogle(googleSignInDto);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -71,6 +97,10 @@ namespace LW.API.Controllers.Public
             [FromBody] FacebookSignInDto facebookSignInDto)
         {
             var result = await _userService.LoginFacebook(facebookSignInDto);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -78,6 +108,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<bool>>> ChangePassword([FromBody] ChangePasswordDto changePasswordDto)
         {
             var result = await _userService.ChangePassword(changePasswordDto);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -85,6 +119,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<bool>>> ForgotPassword([FromBody] string email)
         {
             var result = await _userService.ForgotPassword(email);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -92,6 +130,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<bool>>> ResetPassword([FromBody] ResetPasswordDto resetPasswordDto)
         {
             var result = await _userService.ResetPassword(resetPasswordDto);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -100,6 +142,10 @@ namespace LW.API.Controllers.Public
             [FromBody] TokenRequestDto tokenRequestDto)
         {
             var result = await _userService.RefreshToken(tokenRequestDto);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -107,13 +153,26 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<TokenResponseDto>>> RevokeToken([FromBody] string emailOrUserName)
         {
             var result = await _userService.Revoke(emailOrUserName);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
         
         [HttpPut("UpdateUser")]
         public async Task<ActionResult<ApiResult<UpdateResponseUserDto>>> UpdateUser([FromForm] UpdateUserDto updateUserDto)
         {
+            var validationResult = await new UpdateUserCommandValidator().ValidateAsync(updateUserDto);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult);
+            }
             var result = await _userService.UpdateUser(updateUserDto);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
         
@@ -121,6 +180,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<UserResponseDto>>> GetUser([Required] string userId)
         {
             var result = await _userService.GetUserByUserId(userId);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
     }
