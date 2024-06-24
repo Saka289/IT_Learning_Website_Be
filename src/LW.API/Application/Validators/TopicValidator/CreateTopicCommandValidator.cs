9a2674ea72bba76f5e452e79be7d83c962bb3989
DuyKhanh <caankhwh1303@gmail.com>
2024-06-24 11:20:09 +0700

    update

---
+++ src/LW.API/Application/Validators/TopicValidator/CreateTopicCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using LW.Shared.DTOs.Topic;
+
+namespace LW.API.Application.Validators.TopicValidator;
+
+public class CreateTopicCommandValidator:AbstractValidator<TopicCreateDto>
+{
+    public CreateTopicCommandValidator()
+    {
+        RuleFor(x => x.Title).NotNull().NotEmpty();
+        RuleFor(x => x.Description).NotNull().NotEmpty();
+        RuleFor(x => x.Objectives).NotNull().NotEmpty();
+        RuleFor(x => x.IsActive).NotEmpty();
+        RuleFor(x => x.DocumentId).NotNull().NotEmpty().GreaterThan(0);
+    }
+}
--- src/LW.API/Controllers/Public/TopicController.cs
+++ src/LW.API/Controllers/Public/TopicController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LW.API.Application.Validators.TopicValidator;
 using LW.Services.TopicServices;
 using LW.Shared.DTOs.Level;
 using LW.Shared.DTOs.Topic;
@@ -27,6 +28,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<TopicDto>>> GetAllLevel()
         {
             var result = await _topicService.GetAll();
+            if (!result.IsSucceeded)
+            {
+                return NotFound(result);
+            }
             return Ok(result);
         }
         [HttpGet("GetAllTopicPagination")]
@@ -44,6 +49,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<TopicDto>>> GetTopicById(int id)
         {
             var result = await _topicService.GetById(id);
+            if (!result.IsSucceeded)
+            {
+                return NotFound(result);
+            }
             return Ok(result);
         }
 
@@ -51,20 +60,42 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<IEnumerable<TopicDto>>>> SearchTopic([FromQuery] SearchTopicDto searchTopicDto)
         {
             var result = await _topicService.SearchTopic(searchTopicDto);
+            if (!result.IsSucceeded)
+            {
+                return NotFound(result);
+            }
             return Ok(result);
         }
 
         [HttpPost("CreateTopic")]
         public async Task<ActionResult<ApiResult<bool>>> CreateTopic([FromBody] TopicCreateDto model)
         {
+            var validationResult = await new CreateTopicCommandValidator().ValidateAsync(model);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult);
+            }
             var result = await _topicService.Create(model);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
         [HttpPut("UpdateTopic")]
         public async Task<ActionResult<ApiResult<bool>>> UpdateTopic([FromBody] TopicUpdateDto model)
         {
+            var validationResult = await new UpdateTopicCommandValidator().ValidateAsync(model);
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult);
+            }
             var result = await _topicService.Update(model);
+            if (!result.IsSucceeded)
+            {
+                return BadRequest(result);
+            }
             return Ok(result);
         }
 
@@ -72,6 +103,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<bool>>> UpdateStatusTopic(int id)
         {
             var result = await _topicService.UpdateStatus(id);
+            if (!result.IsSucceeded)
+            {
+                return NotFound(result);
+            }
             return Ok(result);
         }
 
@@ -79,6 +114,10 @@ namespace LW.API.Controllers.Public
         public async Task<ActionResult<ApiResult<bool>>> DeleteTopic(int id)
         {
             var result = await _topicService.Delete(id);
+            if (!result.IsSucceeded)
+            {
+                return NotFound(result);
+            }
             return Ok(result);
         }
     }
