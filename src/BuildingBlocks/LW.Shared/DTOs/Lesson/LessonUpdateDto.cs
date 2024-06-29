406dc035bf7cdd6f763d8b3e2f1c6afadd23cdc5
Saka289 <saka289@icloud.com>
2024-06-29 23:12:43 +0700

    fix: update code

--- src/BuildingBlocks/LW.Shared/DTOs/Lesson/LessonUpdateDto.cs
+++ src/BuildingBlocks/LW.Shared/DTOs/Lesson/LessonUpdateDto.cs
@@ -9,7 +9,7 @@ public class LessonUpdateDto
     public int Id { get; set;  }
     public string Title { get; set; }
     public bool IsActive { get; set; }
-    public string Content { get; set; }
+    public string? Content { get; set; }
     public IFormFile? FilePath { get; set; }
     public int TopicId { get; set; }
 }
