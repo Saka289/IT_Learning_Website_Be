85c4923a768bd0e3cbbac5e888b1749021b9f28e
Saka289 <saka289@icloud.com>
2024-08-07 16:39:34 +0700

    fix: update migration

--- src/Libraries/LW.Data/Persistence/Migrations/20240807093805_Init_Delete_Level.Designer.cs
+++ src/Libraries/LW.Data/Persistence/Migrations/20240807093805_Init_Delete_Level.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LW.Data.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LW.Data.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240807093805_Init_Delete_Level")]
+    partial class Init_Delete_Level
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
