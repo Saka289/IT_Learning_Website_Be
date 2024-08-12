93767dff69a03f2cb6d32b86f4d09f0e08adf4a6
Saka289 <saka289@icloud.com>
2024-08-13 00:22:40 +0700

    fix: update

--- src/BuildingBlocks/LW.Shared/DTOs/Exam/ExamUpdateDto.cs
+++ src/BuildingBlocks/LW.Shared/DTOs/Exam/ExamUpdateDto.cs
@@ -7,11 +7,9 @@ public class ExamUpdateDto
 {
     public int CompetitionId { get; set; }
     public int Id { get; set; }
-    
     public EExamType Type { get; set; } // TL-1 TN-2
     public string? Title { get; set; }
     public string? Province { get; set; }
-    
     public IFormFile? ExamEssayFileUpload { get; set; }
     public IFormFile? ExamSolutionFileUpload { get; set; }
     public string? Description { get; set; }
@@ -19,7 +17,6 @@ public class ExamUpdateDto
     public int NumberQuestion { set; get; }
     public IEnumerable<string>? TagValues { get; set; }
     public bool IsActive { get; set; } = false;
-    
     public int? GradeId { get; set; }
 
 }
