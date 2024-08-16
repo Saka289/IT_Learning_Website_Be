697a4e99f681ce8532463b7205a8839253877188
khanhddq <suttoantruot13.11.2002@gmail.com>
2024-08-16 10:57:19 +0700

    fix: validation

--- src/LW.API/Application/Validators/TopicValidator/UpdateTopicCommandValidator.cs
+++ src/LW.API/Application/Validators/TopicValidator/UpdateTopicCommandValidator.cs
@@ -8,9 +8,9 @@ public class UpdateTopicCommandValidator:AbstractValidator<TopicUpdateDto>
     public UpdateTopicCommandValidator()
     {
         RuleFor(x => x.Id).NotNull().NotEmpty().GreaterThan(0);
-        RuleFor(x => x.Title).NotNull().NotEmpty();
-        RuleFor(x => x.Description).NotNull().NotEmpty();
-        RuleFor(x => x.Objectives).NotNull().NotEmpty();
+        RuleFor(x => x.Title).NotNull().NotEmpty().Length(5, 200);
+        RuleFor(x => x.Description).NotNull().NotEmpty().Length(5, 200);
+        RuleFor(x => x.Objectives).NotNull().NotEmpty().Length(5, 200);
         RuleFor(x => x.IsActive).NotNull();
         RuleFor(x => x.DocumentId).NotNull().NotEmpty().GreaterThan(0);
     }
