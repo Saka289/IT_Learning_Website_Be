97ddc7807b602183fde210c6ac12c1349d7e95b5
khanhddq <suttoantruot13.11.2002@gmail.com>
2024-08-19 10:10:50 +0700

    fix: validate lesson

--- src/LW.API/Application/Validators/LessonValidator/CreateLessonCommandValidator.cs
+++ src/LW.API/Application/Validators/LessonValidator/CreateLessonCommandValidator.cs
@@ -7,7 +7,8 @@ public class CreateLessonCommandValidator : AbstractValidator<LessonCreateDto>
 {
     public CreateLessonCommandValidator()
     {
-        RuleFor(x => x.Title).NotNull().NotEmpty();
+        RuleFor(x => x.Title).NotNull().NotEmpty().Length(5,200);
+        RuleFor(x => x.Content).NotNull().NotEmpty().Length(5,200);
         RuleFor(x => x.TopicId).NotNull().NotEmpty().GreaterThan(0);
         RuleFor(x => x).Must(HaveValidContentOrFilePath).WithMessage("Either Content or FilePath must be provided, but not both.");
     }
