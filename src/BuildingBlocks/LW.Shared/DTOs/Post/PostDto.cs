ae64ece038dfd66226fd994a039fba28b51727c1
DuyKhanh <caankhwh1303@gmail.com>
2024-08-27 17:45:28 +0700

    update

--- src/BuildingBlocks/LW.Shared/DTOs/Post/PostDto.cs
+++ src/BuildingBlocks/LW.Shared/DTOs/Post/PostDto.cs
@@ -8,6 +8,8 @@ public class PostDto
     public string Content { get; set; }
     public string UserId { get; set; }
     public string UserName { get; set; }
+    
+    public string FullName { get; set; }
     public string Avatar { get; set; }
     
     public IEnumerable<string> Roles { get; set; }
